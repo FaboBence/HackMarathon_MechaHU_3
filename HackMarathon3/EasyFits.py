dcf37e7e03b3a6abfb75ec1b86bdc6ee967fba7f
FaboBence <65888378+FaboBence@users.noreply.github.com>
2021-04-29 16:27:58 +0200

    First tries

---
+++ HackMarathon3/EasyFits.py
@@ -0,0 +1,19 @@
+import cv2
+import numpy as np
+import matplotlib.pyplot as plt
+#import scipy.signal as signal
+
+# Image processing
+img = cv2.imread("../marathon-thermofisher-challenge-master/data/train/1 Easy Fits/2018-02-15 17.27.27.162000.tiff",-1)
+#img = cv2.imread("../marathon-thermofisher-challenge-master/data/train/1 Easy Fits/2018-02-15 17.27.27.162000.png",2)
+#print(type(img_tiff[0][0]),"\n",type(img[0][0]))
+plt.imshow(img, cmap="gray", vmin=1000, vmax=1001)
+plt.show()
+ret, thresh16 = cv2.threshold(img,1000,10,cv2.THRESH_BINARY)
+   # Edge detection
+#kernel = np.array([[-1,-1,-1],[-1,8,-1], [-1,-1,-1]])
+thresh8 = thresh16.astype('uint8')
+edges = cv2.Canny(thresh8,9,11)
+plt.imshow(edges, cmap="gray", vmin=0, vmax=1)
+plt.show()
+# Ellipse fitting
--- HackMarathon3/HackMarathon3.py
+++ HackMarathon3/HackMarathon3.py
@@ -1,1 +1,16 @@
+import cv2
+import numpy as np
+import matplotlib.pyplot as plt 
 
+img = cv2.imread("../marathon-thermofisher-challenge-master/data/train/3 Grid Cut-offs/2018-02-15 18.53.26.982000.tiff",-1)
+
+img_blur = cv2.GaussianBlur(img,(39,39),0,borderType = cv2.BORDER_DEFAULT)
+plt.imshow(img_blur, cmap="gray", vmin=0, vmax=50)
+plt.show()
+
+ret,thresh1 = cv2.threshold(img_blur,38,55,cv2.THRESH_BINARY)
+plt.imshow(thresh1, cmap="gray", vmin=0, vmax=55)
+plt.show()
+#print(np.quantile(img,0.99))
+#cv2.imshow("Image",img)
+#cv2.waitKey(10000)
