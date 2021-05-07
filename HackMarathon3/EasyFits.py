3771e4b8238568c18b81ac2e9e2d8f48e6ea6179
FaboBence <65888378+FaboBence@users.noreply.github.com>
2021-05-07 12:35:29 +0200

    Connect Components

--- HackMarathon3/EasyFits.py
+++ HackMarathon3/EasyFits.py
@@ -10,7 +10,7 @@ def pre_process(img):
    # Blur
    img_blur = cv2.GaussianBlur(img,(25,25),0,borderType = cv2.BORDER_DEFAULT)
    # Thresholding
-   ret, thresh16 = cv2.threshold(img_blur,int(max_brightnes/4),10,cv2.THRESH_BINARY)
+   ret, thresh16 = cv2.threshold(img_blur,int(max_brightnes/8),10,cv2.THRESH_BINARY)
       # Edge detection
    #kernel = np.array([[-1,-1,-1],[-1,8,-1], [-1,-1,-1]])
    thresh8 = thresh16.astype('uint8')
@@ -23,6 +23,41 @@ def inner_noise_filtering(edge_img,xc,yc,a,b,theta):
     out_img = cv2.ellipse(edge_img,(int(xc),int(yc)),(int(a),int(b)),int(theta),0,360,(0,0,0),-1)
     return out_img
 
+def curve_detection(img):
+    ret, labels = cv2.connectedComponents(img) # Searching for connected curves
+    curves = []
+    ellipses = []
+    best_fit = None
+    min_R = 10001
+    max_len = 0
+    for i in range(1,ret+1):
+        tmp = np.where(labels == i, 1, 0)
+        sum_tmp = np.sum(tmp)
+        if sum_tmp > 30:
+            curves.append(tmp)
+            if sum_tmp > max_len:
+                best_fit = curves[-1]
+                max_len = sum_tmp
+    try:
+
+        xc,yc,a,b,theta,R_square = fit_ellipse(best_fit)
+        best_fit = [xc,yc,a,b,theta,R_square]
+    except:
+        best_fit = None
+    # Fitting an ellipse on every curve
+    #for curve in curves:
+    #    try:
+    #        xc,yc,a,b,theta,R_square = fit_ellipse(curve)
+    #        ellipses.append([xc,yc,a,b,theta,R_square])
+    #        if R_square < min_R:
+    #            best_fit = ellipses[-1]
+    #            min_R = R_square
+    #    except:
+    #        pass
+    if best_fit:
+        return best_fit[:]
+    return best_fit
+    
    # Ellipse fitting
 def fit_ellipse(edges):
    edges_coord = np.where(edges > 0)
@@ -36,10 +71,10 @@ def fit_ellipse(edges):
       xc, yc, a, b, theta = ell.params
       theta = theta*180/3.14
       R_square = np.average(np.square(ell.residuals(edge_poins)))
-      print(R_square)
+      #print(R_square)
       if R_square > 10000:
           return None # No ellipse found
-      return xc, yc, a, b, theta
+      return xc, yc, a, b, theta, R_square
    except:
       return None 
 
--- HackMarathon3/HackMarathon3.py
+++ HackMarathon3/HackMarathon3.py
@@ -12,14 +12,23 @@ if __name__ == "__main__":
     results = []
     for i,tiff in enumerate(tiffs):
         start = time.time() # Measuring elapsed time
+        # Edge detection
         done.append(pre_process(tiff))
         try:
-            xc, yc, a, b, theta = fit_ellipse(done[-1])
+            # First ellipse fit on raw edge detection data
+            xc, yc, a, b, theta, R_square = fit_ellipse(done[-1])
             cv2.ellipse(pngs[i],(int(xc),int(yc)),(int(a),int(b)),int(theta),0,360,(255,0,0),3)
+            print(R_square)
+
+            # Noise filtering
             filtered_img = inner_noise_filtering(deepcopy(done[-1]),xc,yc,a,b,theta)
             filtered.append(filtered_img)
-            xc, yc, a, b, theta = fit_ellipse(filtered_img)
-            
+            xc, yc, a, b, theta, R_square = fit_ellipse(filtered_img)
+            print(R_square)
+
+            # Curve detection
+            xc, yc, a, b, theta, R_square = curve_detection(filtered_img)
+
             cv2.ellipse(pngs[i],(int(xc),int(yc)),(int(a),int(b)),int(theta),0,360,(0,255,0),3)
             end = time.time() # Measuring elapsed time
             results.append([names[i],xc,yc,a,b,theta,(end-start)*1000]) # Saving results
